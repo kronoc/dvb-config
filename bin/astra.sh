853875dea56d4b234b280db0c1e398883f2ddc00
conor.keegan <con@conor.net>
2020-11-08 13:25:11 +0000

    cleaning things up, adding some generic astra config

---
+++ bin/astra.sh
@@ -0,0 +1,1 @@
+dvblast -a $(./find_rx DVB-S 1) -f 11671750 -s 23000000 -C -M CNN -F 23 -c ../config/Astra.cfg -v 18 -m psk_8
--- bin/mux-bbc.sh
+++ bin/mux-bbc.sh
@@ -1,2 +1,2 @@
-dvblast -a $(./find_rx DVB-S 1) -f 10788000 -s 22000000 -c ../config/Astra.cfg -v 13 -5 DVBS
+dvblast -a $(./find_rx DVB-S 1) -f 10788000 -s 22000000 -c ../config/BBC.cfg -v 13 -5 DVBS
 
